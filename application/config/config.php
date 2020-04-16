273a7726752cb6b3385f26f0325ce1ab38967ef2
namratashrivas <namrata.r.shrivas@gmail.com>
2020-04-16 14:14:12 +0530

    config changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/covid-19';
+$config['base_url'] = 'http://localhost/patient_management';
 
 /*
 |--------------------------------------------------------------------------
